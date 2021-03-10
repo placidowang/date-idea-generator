47675e5570264ce5a546dcfe13999304271061d7
Placido Wang <placidow@gmail.com>
2021-03-10 00:05:15 -0600

    adding app module import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CategoriesComponent } from './categories/categories.component';
@@ -11,6 +12,7 @@ import { CategoriesComponent } from './categories/categories.component';
   ],
   imports: [
     BrowserModule
+    // FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
