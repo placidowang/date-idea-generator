f78c98c68d7e53c91eac27d5a7b55de21435cae8
Placido Wang <placidow@gmail.com>
2021-03-14 23:59:27 -0500

    creating Categories

---
+++ src/app/categories.ts
@@ -0,0 +1,11 @@
+import { Category } from './category';
+
+export const CATEGORIES: Category[] = [
+  { id: 1, name: 'Simple'},
+  { id: 2, name: 'Active'},
+  { id: 3, name: 'Night Out'},
+  { id: 4, name: 'Multi-Day'},
+  // { id: 5, name: 'Simple'},
+  // { id: 6, name: 'Simple'},
+  
+];
