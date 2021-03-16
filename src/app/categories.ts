ed40541d7aa05308941c00fd4901f821c36e6b3a
Placido Wang <placidow@gmail.com>
2021-03-15 23:56:05 -0500

    updating categories

--- src/app/categories.ts
+++ src/app/categories.ts
@@ -3,9 +3,9 @@ import { Category } from './category';
 export const CATEGORIES: Category[] = [
   { id: 1, name: 'Simple'},
   { id: 2, name: 'Active'},
-  { id: 3, name: 'Night Out'},
-  { id: 4, name: 'Multi-Day'},
-  // { id: 5, name: 'Simple'},
-  // { id: 6, name: 'Simple'},
+  { id: 3, name: 'Skills/Crafts'},
+  { id: 4, name: 'Night Out'},
+  { id: 5, name: 'Whole Day'},
+  { id: 6, name: 'Multi-Day'},
   
 ];
