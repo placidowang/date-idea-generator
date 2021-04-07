293cd8bdcc186bdf08a43b56ed99ccc5cc041035
Placido Wang <placidow@gmail.com>
2021-04-07 11:43:06 -0500

    inject message service into category service

--- src/app/category.service.ts
+++ src/app/category.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 import { Category } from './category';
 import { CATEGORIES } from './categories';
-
-import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getCategories(): Observable<Category[]> {
     const categories = of(CATEGORIES);
+    this.messageService.add('CategoryService: fetched categories');
     return categories;
   }
 }
